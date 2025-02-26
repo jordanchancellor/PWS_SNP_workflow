b9da9bda89831b78e847f37114d684d9d7de0b85
jordanchancellor <jordan.chancellor.15@gmail.com>
2025-02-26 13:28:13 +0100

    updatescripts

--- alignment.sh
+++ alignment.sh
@@ -1,117 +1,126 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#SBATCH -J bowtie
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=50G
+#SBATCH --time=7-00:00:00
+#SBATCH --constraint=cal
+#SBATCH --error=%x_%a.err
+#SBATCH --output=%x_%a.out
+#SBATCH --array=1-45
 
-# a conda-dependent bash script to map fastq files to reference genome using bwa
-# usage: bash PWS_SNP_workflow/alignment.sh
-# assumes you have conda already installed and environments created
-
-# define resources and batch sizes (us batch size of 1 to run one file at a time in loop)
-THREADS="2"
-your_email="jordan.chancellor.15@gmail.com"
-batch_size=24 
-count=0
-jobs=()
+# a module-dependent bash script to map fastq files to reference genome using bowtie2
+# usage: sbatch PWS_SNP_workflow/alignment.sbatch <aligner>
 
 # define global variables
-wd="/home/jordan/project"
-log_out="${wd}/trimmomatic.out"
-log_err="${wd}/trimmomatic.err"
+THREADS=${SLURM_CPUS_PER_TASK:-12}
+aligner=$1
+wd="/mnt/scratch/users/agr_3216_ifapa/alorenzo/vannamei_snp_workflow"
 samples_file="${wd}/samples_file.txt"
-readsdir="${wd}/reads/trimmed"
+sample_id=$(cat $samples_file | sed -n ${SLURM_ARRAY_TASK_ID}p)
+reads="${wd}/reads/trimmed"
 outdir="${wd}/alignment"
-index="${wd}/indices/pvannamei/GCF_042767895.1_ASM4276789v1_genomic.fna.gz"
+read1="${reads}/${sample_id}_F_trimmed_paired.fq.gz"
+read2="${reads}/${sample_id}_R_trimmed_paired.fq.gz"
+reference="${wd}/indices/pvannamei/GCF_042767895.1_ASM4276789v1_genomic.fna.gz" # Genome assembly ASM4276789v1
+index=$(basename "${reference}" .fna.gz)
+samfile="${outdir}/${sample_id}.sam"
+sortedbamfile="${outdir}/${sample_id}_sorted.bam"
 
-# Load conda environment
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate mapping
+# extract read group information from fastq's
+ID="$(zcat $read1 | head -n 1 | cut -d':' -f3,4 | tr ':' '.')"
+PU="$ID"
+LB="$(zcat $read1 | head -n 1 | awk '{print $2}' | cut -f 4 -d ":")"
+PL="ILLUMINA"
+
+# Load  environment
+module purge
+module load samtools/1.21
+module load gatk/4.4.0.0
+
+# check if aligner is specified
+if [ -z "$1" ]; then
+    echo "Error: No aligner specified."
+    echo "Usage: PWS_SNP_workflow/alignment.sbatch <aligner>"
+    echo "Options: bwa, bowtie"
+    exit 1
+fi
 
 # create output directory (if needed)
-if [[ ! -d $outdir ]]
-then
+if [[ ! -d $outdir ]]; then
 	printf "Creating output directory %s\n" "${outdir}"
 	mkdir $outdir 
 fi
 
-# BWA checkpoint
-bwa mem -version 2>> "$log_err" | tee -a "$log_out"
-if [[ $? -ne 0 ]]; then
-    echo "Error: trimmomatic not installed correctly." | tee -a "$log_err"
-    exit 1
-fi
+# align reads as determined by aligner options
+case "$aligner" in
+    bowtie)
+        echo "Running alignment using bowtie2"
+        module load bowtie/2.5.1
+        # bowtie2 install checkpoint
+        bowtie2 --version || { echo "Error: Bowtie2 not installed correctly."; exit 1; }
 
-# align read pairs using bwa in batches 
-while read -r sample_id; do
-    read1="${readsdir}/${sample_id}_1.fastq.gz"
-    read2="${readsdir}/${sample_id}_2.fastq.gz"
+        # check for existence of index files, if none exist, create index
+        if [[ ! -f ${index}.1.bt2 ]]; then
+            echo "Bowtie2 index not found. Creating index..."
+            bowtie2-build --threads $THREADS -f \
+            ${reference} ${index}
+        fi
 
-    # Check if both read files exist
-    if [[ ! -f "$read1" || ! -f "$read2" ]]; then
-        echo "Error: Missing files for ${sample_id}, skipping." | tee -a "$log_err"
-        continue
-    fi
-    # Set up log files for each individual job
-    job_out="${sample_id}_mapping.out"
-    job_err="${sample_id}_mapping.err"
+        echo "Aligning ${read1} and ${read2} to index ${index} using bowtie2." 
 
-    # Run bwa on paired-end reads
-    echo "Running bwa on files associated with ${sample_id}: ${read1} and ${read2}" | tee -a "$log_out"
+        # align reads using bowtie2
+        bowtie2 -p $THREADS --no-discordant \
+        -x "${index}" \
+        --rg-id "$ID" --rg "SM:$sample_id" --rg "LB:$LB" --rg "PL:$PL" \
+        -1 $read1 -2 $read2 \
+        -S $samfile
+        echo "Bowtie2 mapping completed. Mapping files can be found in ${outdir}."
+        ;;
+    bwa)
+        echo "Running alignment using bwa mem"
+        module load bwa/0.7.17
+        # BWA install checkpoint
+        bwa 2>&1 | head -n 1 || { echo "Error: BWA not installed correctly."; exit 1; }
 
-    bwa mem -t "$THREADS" \
-    "${index}" "${read1}" "${read2}" \
-    > "${outdir}/${sample_id}.sam" 2> "$job_out" 3> "$job_err" &
+        # check for existence of index files, if none exist, create index
+        if [[ ! -f ${index}.amb  ]]; then
+        bwa index -p ${index} \
+        -a bwtsw \
+        ${reference}
+        fi
 
-    jobs+=($!)
+        echo "Aligning ${read1} and ${read2} to index ${index} using bwa mem."
 
-    count=$((count + 1))
+        # align reads using bwa mem
+        bwa mem -t $THREADS \
+        -R "@RG\tID:$ID\tSM:$sample_id\tLB:$LB\tPL:$PL" \
+        $index $read1 $read2 > "${samfile}"
+        echo "BWA mapping completed. Mapping files can be found in ${outdir}."
+        ;;
+    *)
+        echo "Error: Invalid aligner '$aligner'."
+        echo "Usage: PWS_SNP_workflow/alignment.sbatch <aligner>"
+        echo "Options: bwa, bowtie"
+        exit 1
+        ;;
+esac
 
-    if [[ $count -ge $batch_size ]]; then
-        wait "${jobs[@]}"
-        jobs=()
-        count=0
-    fi
-
-done < "$samples_file"
-
-wait
-echo "BWA mapping completed. Mapping files can be found in ${outdir}." | tee -a "$log_out"
-echo "BWA mapping completed. Mapping files can be found in ${outdir}." | ssmtp "$your_email"
-
-# convert sam files to sorted bam files, remove sam files (for the sake of saving space)
-
-
-############## below is scratch from an old script, I am working on updating the above
-
-bwacmd="bwa mem -t $SLURM_CPUS_PER_TASK -R $(echo "@RG\tID:$id\tSM:$sm\tLB:$lb\tPL:ILLUMINA") $index $reads1 $reads2"
-
-echo "$bwacmd" 
-
-$bwacmd > "${outdir}/${sample_id}.sam"
-
-conda deactivate
-conda activate samtools
-
+# convert sam files to indexed bam files 
 echo "Converting sam files to sorted bam files with samtools."
+samtools view -bh -f 2 --threads $THREADS ${samfile} | samtools sort -m 30G -@ $THREADS -o ${sortedbamfile}
 
-samfile="${outdir}/${sample_id}.sam"
-
-cat ${samfile} | samtools view -bSh | samtools sort -o ${outdir}/${sample_id}.sorted.bam
-
-echo "Creating samtools index file from ${sortedbamfile}"
-
+# index bam file
+echo "Indexing ${sortedbamfile} with samtools."
 samtools index -b ${sortedbamfile} 
 
-echo "Running idxstats on sorted, indexed bam file from ${sortedbamfile}"
+# get mapping statistics from bam file
+echo "Creating mapping rate file with samtools flagstat from ${sortedbamfile}"
+samtools flagstat ${sortedbamfile} > ${outdir}/${sample_id}_mappingstats.txt
 
-samtools idxstats ${sortedbamfile} | tee ${outdir}/${sample_id}.idxstats.txt
-
-echo "Creating mapping rate file with samtools flagstat from ${bamfile}"
-
-samtools flagstat ${sortedbamfile} > ${outdir}/${sample_id}.flagstat.txt
-
-conda deactivate
-conda activate gatk
-
-# Run GATK4's ValidateSamFile in summary mode
+# run GATK4's ValidateSamFile in summary mode
 gatk ValidateSamFile --INPUT ${sortedbamfile} \
---REFERENCE_SEQUENCE ${index} \
+--REFERENCE_SEQUENCE ${reference} \
 --MODE SUMMARY \
---OUTPUT ${outdir}/${sample_id}.validate.summary
+--OUTPUT ${outdir}/${sample_id}_initial_validate_summary.txt
+
